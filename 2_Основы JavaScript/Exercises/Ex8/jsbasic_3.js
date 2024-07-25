b34ca56d8d4fc87117a12a2bb23a9e9057402340
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-07-25 14:51:13 +0300

    [Practice][2nd Module] 5th completed

--- 2_Основы JavaScript/Exercises/Ex8/jsbasic_3.js
+++ 2_Основы JavaScript/Exercises/Ex8/jsbasic_3.js
@@ -1,13 +1,13 @@
 {
-"use strict";
-let counter = 10;
+    "use strict";
+    let counter = 10;
 
-let countdown = () => {
-    document.getElementById('cont').innerHTML = counter--;
-    if (counter < 0) {
-        clearInterval(interval_id);
+    let countdown = () => {
+        document.getElementById('cont').innerHTML = counter--;
+        if (counter < 0) {
+            clearInterval(interval_id);
+        }
     }
-}
 
-let interval_id = setInterval(countdown, 1000); //запустить таймер с интервалом в 1 секунду и передать первым аргументом countdown.
+    let interval_id = setInterval(countdown, 1000); //запустить таймер с интервалом в 1 секунду и передать первым аргументом countdown.
 }
