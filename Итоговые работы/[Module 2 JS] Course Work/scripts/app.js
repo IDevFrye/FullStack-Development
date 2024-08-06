383adb6342e8252143f0dfa7462377adc7e44887
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-08-07 01:12:59 +0300

    [CW2] Markup + Menu rendering

---
+++ Итоговые работы/[Module 2 JS] Course Work/scripts/app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+
+/* page */
+const page = {
+    menu: document.querySelector('.menu__list')
+}
+
+/* utils */
+//Загрузка данных
+function loadData() {
+    const habbitsString = localStorage.getItem(HABBIT_KEY);
+    const habbitArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
+    }
+}
+
+//Сохранение данных
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+//Рендеринг
+function rerenderMenu(activeHabbit) {
+    if (!activeHabbit) {
+        return;
+    }
+    for (const habbit of habbits) {
+        const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+        if (!existed) {
+            const element = document.createElement('button');
+            element.setAttribute('menu-habbit-id', habbit.id);
+            element.classList.add('menu__item');
+            element.addEventListener('click', () => {
+                rerender(habbit.id);
+            })
+            element.innerHTML = `<img src="./images/${habbit.icon}.svg" alt="${habbit.name}">`;
+            if (activeHabbit.id === habbit.id){
+                element.classList.add('menu__item_active');
+            }
+            page.menu.appendChild(element);
+            continue;
+        }
+        if (activeHabbit.id === habbit.id){
+            existed.classList.add('menu__item_active');
+        } else {
+            existed.classList.remove('menu__item_active');
+        }
+    }
+}
+
+function rerender(activeHabbitId) {
+    const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+    rerenderMenu(activeHabbit);
+}
+
+//Инциализация
+(() => {
+    loadData();
+    rerender(habbits[0].id);
+})()
+
